190cad25bdf1c357643b7d2c69539f7303018629
MagnusIIIBR <nathan.mauclene@gmail.com>
2018-04-17 22:18:55 -0300

    Criação do DbContext

---
+++ ComplineProva.Data/Context/ComplineDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ComplineProva.Domain;
+using ComplineProva.Data.Mapping;
+
+namespace ComplineProva.Data.Context
+{
+    public class ComplineDataContext : DbContext
+    {
+        public ComplineDataContext()
+            : base("ComplineConnectionString")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new TarefaMap());
+        }
+    }
+
+    public class ComplineDbContextInitializer : DropCreateDatabaseAlways<ComplineDataContext>
+    {
+    }
+}
